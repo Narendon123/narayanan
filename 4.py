4c577dcdc986f1274d2085bb5723dbcbe43ea5d4
Narendon123 <51222534+Narendon123@users.noreply.github.com>
2019-05-31 15:22:38 +0530

    4.py

---
+++ 4.py
@@ -0,0 +1,10 @@
+n=input()
+if(n=='a' or n=='A' or n=='e' or n=='E' or n=='i' or n=='I' or n=='o' or n=='O' or n=='u' or n=='U'):
+  print("Vowel")
+elif(n=='@' or n=='#' or n=='$' or n=='%' or n=='^' or n=='&' or n=='*' or n=='(' or n==')'):
+  print("Invalid")
+else:
+  print("Consonant")
+  
+
+
